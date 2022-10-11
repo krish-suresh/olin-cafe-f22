9fc82020ba32b21cce9fc28bc2100ec3f86d8d58
Krishna Suresh <krishna4132@gmail.com>
2022-10-11 13:34:51 -0400

    conway cell progress

--- labs/01_game_of_life/hdl/conway_cell.sv
+++ labs/01_game_of_life/hdl/conway_cell.sv
@@ -21,32 +21,27 @@ All other live cells die in the next generation. Similarly, all other dead cells
 // Sum neighbors with 2 bit adder
 // state_0 & (sum & 10 | sum & 11) & ~c_out
 // ~state_0 & ((sum & 11) & ~c_out)
-wire [1:0] s0,s1,s2,s3,s4,s5,s6;
+logic [1:0] s0,s1,s2,s3,s4,s5,s6;
+wire c0,c1,c2,c3,c4,c5,c6,c7;
 logic c_out;
 logic [1:0] sum;
-always_ff @(posedge clk)
-state_q <= state_d;
-// adder_n UUT(2'b0,{neighbors[0],1'b0}, 1'b0, s0, c_out);
-// adder_n UUT1(s0, {neighbors[1],1'b0}, 1'b0, s1, c_out);
-// adder_n UUT2(s1, {neighbors[2],1'b0}, 1'b0, s2, c_out);
-// adder_n UUT3(s2, {neighbors[3],1'b0}, 1'b0, s3, c_out);
-// adder_n UUT4(s3, {neighbors[4],1'b0}, 1'b0, s4, c_out);
-// adder_n UUT5(s4, {neighbors[5],1'b0}, 1'b0, s5, c_out);
-// adder_n UUT6(s5, {neighbors[6],1'b0}, 1'b0, s6, c_out);
-// adder_n UUT7(s6, {neighbors[7],1'b0}, 1'b0, sum, c_out);
+adder_n ADDER({1'b0,neighbors[0]}, {1'b0,neighbors[1]},1'b0, s0, c0);
+adder_n UUT1(s0, {1'b0, neighbors[2]}, 1'b0, s1, c1);
+adder_n UUT2(s1, {1'b0, neighbors[2]}, 1'b0, s2, c2);
+adder_n UUT3(s2, {1'b0, neighbors[3]}, 1'b0, s3, c3);
+adder_n UUT4(s3, {1'b0, neighbors[4]}, 1'b0, s4, c4);
+adder_n UUT5(s4, {1'b0, neighbors[5]}, 1'b0, s5, c5);
+adder_n UUT6(s5, {1'b0, neighbors[6]}, 1'b0, s6, c6);
+adder_n UUT7(s6, {1'b0, neighbors[7]}, 1'b0, sum, c7);
+// always @(sum or c_out) begin
+//     $display("s(%d %b", {c_out,sum},  neighbors);
+// end
 always_comb begin
-    sum = 11;
-    c_out = 0;
-    state_d = ~rst & ((state_0 & (sum & 10 | sum & 11) & ~c_out) | (~state_0 & ((sum & 11) & ~c_out)));
-
-    // r = ~clk & ~state_d;
-    // s = ~clk & state_d;
-    // q_m = r ~| (s ~| q_m);
-
-    // r_s = clk & ~q_m;
-    // s_s = clk & q_m;
-    // state_q = r ~| (s ~| state_q);
+    c_out = c0|c1 |c2|c3|c4|c5|c6|c7;
+    state_d = ~rst & ((state_0 & ((sum & 10) | (sum & 11)) & ~c_out) | (~state_0 & ((sum & 11) & ~c_out)));
 end
+always_ff @(posedge clk)
+    state_q <= state_d;
 
 
 endmodule
