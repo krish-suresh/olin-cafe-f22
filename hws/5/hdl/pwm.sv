57da9aca87650c4c09b57b16eac5208e8f177433
Krishna Suresh <krishna4132@gmail.com>
2022-10-25 20:40:22 -0400

    pwm done

--- hws/5/hdl/pwm.sv
+++ hws/5/hdl/pwm.sv
@@ -13,8 +13,7 @@ input wire [N-1:0] duty; // The "duty cycle" input.
 output logic out;
 
 logic [N-1:0] counter;
-logic [N-1:0] ticks; // The "duty cycle" input.
-logic ff;
+
 // Create combinational (always_comb) and sequential (always_ff @(posedge clk)) 
 // logic that drives the out signal.
 // out should be off if ena is low.
@@ -23,14 +22,35 @@ logic ff;
 // bonus: out should be fully zero at duty = 0, and fully 1 (always on) at duty = 2^N-1;
 // You can use behavioural combinational logic, but try to keep your sequential
 //   and combinational blocks as separate as possible.
+logic [N-1:0] ticks;
+initial counter = 0;
 initial out = 0;
 always_comb ticks = out ? duty : 2**N-1-duty;
-pulse_generator #(.N(N)) GEN(.clk(step), .rst(rst), .ena(ena), .ticks(ticks), .out(ff));
-always_ff @( posedge clk) begin
+always_ff @( posedge step ) begin
   if (rst) begin
-    out <= 0;
-  end else begin
-    out <= ~out & ena;
+    counter <= 0;
+    ticks <= duty;
+    out <=0;
+  end
+  else if(ena) begin 
+      if (counter == ticks & duty != 0 & duty != 2**N-1) begin 
+        out <= ~out;
+        counter <= 0;
+      end  
+      else begin
+        // out <= 0;
+        counter <= counter+1;
+      end 
   end
 end
+// initial out = 0;
+// always_comb ticks = out ? duty : 2**N-1-duty;
+// pulse_generator #(.N(N)) GEN(.clk(step), .rst(rst), .ena(ena), .ticks(ticks), .out(ff));
+// always_ff @( posedge clk) begin
+//   if (rst) begin
+//     out <= 0;
+//   end else begin
+//     out <= ~out & ena;
+//   end
+// end
 endmodule
